856404128ab847a232cfa9649fe8b5a11a818faf
mluyten <matt.luyten@gmail.com>
2024-11-06 11:47:27 -0500

    Added lab3 code I missed

--- ECE4122-LAB3/code/Model.h
+++ ECE4122-LAB3/code/Model.h
@@ -4,7 +4,7 @@ Class: ECE4122
 Last Date Modified: 10/21/2024
 
 Description:
-
+This is the header for the model class. It is a utility for loading meshes
 */
 
 #pragma once
@@ -17,6 +17,8 @@ using namespace glm;
 #include <common/objloader.hpp>
 #include <common/texture.hpp>
 
+// This class represents one mesh. The mesh is loaded in from a .obj file and textured with a .dds file. It can be loaded at any
+// position or angle around the z axis
 class Model {
 public:
 	Model (std::string modelPath, std::string texturePath, int index=0,  glm::vec3 pos=glm::vec3(0,0,0), float ang=0);
@@ -31,6 +33,7 @@ private:
 	std::vector<glm::vec2> indexed_uvs;
 	std::vector<glm::vec3> indexed_normals;
 
+    // Desired angle and position of mesh
 	glm::vec3 position;
 	float angle;
 
